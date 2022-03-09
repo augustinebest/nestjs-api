a0e7f6bf68d7e47495f2aecbd80609c68f8b994d
augustinebest <augustusbest00@gmaile.com>
2022-03-10 00:47:06 +0100

    create BookmarkService

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,44 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { createBookmarkDto, EditBookmarkDto } from './dto';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+  getBookmarks(userId: number) {
+    return this.prisma.bookmark.findMany({ where: { userId } });
+  }
+
+  async createBookmark(userId: number, dto: createBookmarkDto) {
+    const bookmark = await this.prisma.bookmark.create({
+      data: { ...dto, userId },
+    });
+    return bookmark;
+  }
+
+  async updateBookmark(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {
+    // get the bookmark by id
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    // check if user owns the bookmark
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException('Access to resources denied');
+
+    return this.prisma.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+}

ead939ef274840f7f5700cab755f56bd40ca64e1
augustinebest <augustusbest00@gmaile.com>
2022-03-10 00:48:20 +0100

    added service to update a user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorators';
 import { JWTGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JWTGuard)
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('profile')
   // profile(@Req() req: Request)
-  profile(@GetUser() user: User) {
-    console.log(user);
-    return 'user info';
+  profile(@GetUser('email') email: string) {
+    return this.userService.getUser(email);
+    // return user;
+  }
+
+  @Patch('profile')
+  update(@GetUser('sub') userId: number, @Body() dto: EditUserDto) {
+    try {
+      return this.userService.updateProfile(userId, dto);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }

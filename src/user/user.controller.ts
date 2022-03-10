0ff6ae9757eade9c61e4dd1f48534ea14faec324
augustinebest <augustusbest00@gmaile.com>
2022-03-10 10:49:27 +0100

    removed User prisma client

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorators';
 import { JWTGuard } from '../auth/guard';
 import { EditUserDto } from './dto';
